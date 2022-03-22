a7e2bfdee281484a98035db5daab3285ad2c336d
Marc Juul <marc@juul.io>
2022-03-21 19:10:01 -0700

    progress

--- client.sh
+++ client.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
 
-SERVER=https://localhost:8080/request
-CONF=/etc/wireguard/wg0.conf      
-PRIVKEY=/etc/wireguard/private.key
-PUBKEY=/etc/wireguard/public.key
-              
-touch $PRIVKEY    
-chmod 600 $PRIVKEY  
-wg genkey > $PRIVKEY              
-cat $PRIVKEY | wg pubkey > $PUBKEY
-           
+SERVER_URL="https://localhost:8080/request"
+WIREGUARD_CONFIG_DIR=./conf # for testing
+#WIREGUARD_CONFIG_DIR=./etc
+CONF=${WIREGUARD_CONFIG_DIR}/wg0.conf      
+PRIVKEY_FILE=${WIREGUARD_CONFIG_DIR}/private.key
+PUBKEY_FILE=${WIREGUARD_CONFIG_DIR}/public.key
+
+mkdir -p $WIREGUARD_CONFIG_DIR
+
+# TODO check if files already exist
+
+touch $PRIVKEY_FILE
+chmod 600 $PRIVKEY_FILE  
+wg genkey > $PRIVKEY_FILE              
+PUBKEY=$(cat $PRIVKEY_FILE | wg pubkey)
+echo $PUBKEY > $PUBKEY_FILE
+
 touch $CONF    
 chmod 600 $CONF            
-echo "[Interface]" >> $CONF                 
-echo "PrivateKey = $(cat $PRIVKEY)" >> $CONF
+echo "[Interface]" > $CONF                 
+echo "PrivateKey = $PRIVKEY" >> $CONF
 
-RESPONSE=$(wget --quiet $SERVER -O -) 
+RESPONSE=$(wget --quiet ${SERVER_URL}/${PUBKEY} -O -) 
--- server.py
+++ server.py
@@ -1,16 +1,40 @@
 #!/usr/bin/env python3
 
+import subprocess
 import wgconfig
 from bottle import route, run, template
+
+PORT=8080
+CLIENT_SUBNET="24"
 
 wc = wgconfig.WGConfig('./example.conf')
 wc.read_file()
 privkey = wc.interface['PrivateKey']
+res = subprocess.run(["wg", "pubkey"], input=privkey, text=True, capture_output=True)
+# TODO error check
+pubkey = res.stdout
 
-print("Priv:", privkey)
+# TODO stub
+def findNextClientIP():
+    return "100.70.0.1"
 
-@route('/hello/<name>')
-def index(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
+
+def addClientToConfig(clientPubKey, clientRequestIP="unknown IP"):
+    global CLIENT_SUBNET
+    clientIP = findNextClientIP()
+
+    wc.add_peer(clientPubKey, "# Added by " + clientRequestIP)
+    wc.add_attr(clientPubKey, "AllowedIPs", clientIP + "/" + CLIENT_SUBNET)
+
+    wc.write_file()
+
+    return clientIP
+
+@route('/request/<key>')
+def index(key):
+    clientIP = addClientToConfig(key)
+    # TODO return text instead of HTML
+    return template('{{clientIP}} {{serverPubkey}}', clientIP=clientIP, serverPubkey=pubkey)
+
 
 run(host='localhost', port=8080)
