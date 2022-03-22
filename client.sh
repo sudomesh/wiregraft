9bd6156572c3307090c9174f4e236c5bccb57602
Marc Juul <marc@juul.io>
2022-03-21 22:28:15 -0700

    Seems to be basically working!

--- client.sh
+++ client.sh
@@ -2,11 +2,19 @@
 #!/bin/sh
 
 SERVER_URL="http://localhost:8080/request"
+WIREGUARD_ENDPOINT="example.com:49992"
+
+CLIENT_ALLOWEDIPS="172.25.0.0/24" # TODO should come from server
+KEEPALIVE="10" # TODO should come from server
+
 WIREGUARD_CONFIG_DIR=./conf # for testing
 #WIREGUARD_CONFIG_DIR=./etc
+
 CONF=${WIREGUARD_CONFIG_DIR}/wg0.conf      
 PRIVKEY_FILE=${WIREGUARD_CONFIG_DIR}/private.key
 PUBKEY_FILE=${WIREGUARD_CONFIG_DIR}/public.key
+
+###################################################
 
 mkdir -p $WIREGUARD_CONFIG_DIR
 
@@ -14,7 +22,8 @@ mkdir -p $WIREGUARD_CONFIG_DIR
 
 touch $PRIVKEY_FILE
 chmod 600 $PRIVKEY_FILE  
-wg genkey > $PRIVKEY_FILE              
+PRIVKEY=$(wg genkey)
+echo $PRIVKEY > $PRIVKEY_FILE              
 PUBKEY=$(cat $PRIVKEY_FILE | wg pubkey)
 echo $PUBKEY > $PUBKEY_FILE
 
@@ -22,9 +31,10 @@ touch $CONF
 chmod 600 $CONF            
 echo "[Interface]" > $CONF                 
 echo "PrivateKey = $PRIVKEY" >> $CONF
+echo " " >> $CONF
 
 RESPONSE=$(curl --silent --data-urlencode key=${PUBKEY} ${SERVER_URL}) 
-#echo $RESPONSE
+echo "Got response: $RESPONSE"
 LEN=${#RESPONSE}
 i=3
 
@@ -39,8 +49,8 @@ while [ "$i" -lt "$LEN" ]; do
     i=$((i+1))
 done
 
-echo $ASSIGNED_IP
-echo $SERVER_PUBKEY
-
-
-
+echo "[Peer]" >> $CONF
+echo "Endpoint = $WIREGUARD_ENDPOINT" >> $CONF
+echo "AllowedIPs = $CLIENT_ALLOWEDIPS" >> $CONF
+echo "PublicKey = $SERVER_PUBKEY" >> $CONF
+echo "PersistenKeepalive = $KEEPALIVE" >> $CONF
